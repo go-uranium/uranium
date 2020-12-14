fa6464128946a5404a6a5633abfaa314af624436
Richard Chen <i@iochen.com>
2020-12-14 12:52:49 +0800

    add more session funcs
    Signed-off-by: Richard Chen <i@iochen.com>

--- data/session.go
+++ data/session.go
@@ -9,9 +9,11 @@ import (
 
 var (
 	SQLSessionByToken = `SELECT token, uid, UA, IP, time, expire_at FROM ushio.session WHERE token=?;`
-	SQLSessionByUID   = `SELECT token, uid, UA, IP, time, expire_at FROM ushio.session WHERE uid=?;`
+	SQLSessionsByUID  = `SELECT token, uid, UA, IP, time, expire_at FROM ushio.session WHERE uid=?;`
 
-	SQLInsertSession = `INSERT INTO ushio.session(token, uid, UA, IP, time, expire_at) VALUES (?,?,?,?,?,?);`
+	SQLInsertSession   = `INSERT INTO ushio.session(token, uid, UA, IP, time, expire_at) VALUES (?,?,?,?,?,?);`
+	SQLDisableSessions = `UPDATE ushio.session SET expire_at = NOW() WHERE uid=?;`
+	SQLDeleteSessions  = `DELETE FROM ushio.session WHERE uid=?;`
 )
 
 func SessionByToken(token string) (*session.Session, error) {
@@ -25,8 +27,8 @@ func SessionByToken(token string) (*session.Session, error) {
 	return s, nil
 }
 
-func SessionByUID(uid int) ([]*session.Session, error) {
-	rows, err := db.Query(SQLSessionByUID, uid)
+func SessionsByUID(uid int) ([]*session.Session, error) {
+	rows, err := db.Query(SQLSessionsByUID, uid)
 	if err != nil {
 		return nil, err
 	}
@@ -47,6 +49,22 @@ func SessionByUID(uid int) ([]*session.Session, error) {
 
 func InsertSession(s *session.Session) error {
 	_, err := db.Exec(SQLInsertSession, s.Token, s.UID, s.UA, s.IP, s.Time, s.Expire)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DisableSessions(uid int) error {
+	_, err := db.Exec(SQLDisableSessions, uid)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteSessions(uid int) error {
+	_, err := db.Exec(SQLDeleteSessions, uid)
 	if err != nil {
 		return err
 	}
