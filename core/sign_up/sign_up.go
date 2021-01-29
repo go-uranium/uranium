06e250b1dfe2622274fdbc1ffffd3df8e52b9389
Richard Chen <i@iochen.com>
2021-01-29 17:08:33 +0800

    add SignUp type
    Signed-off-by: Richard Chen <i@iochen.com>

---
+++ core/sign_up/sign_up.go
@@ -0,0 +1,46 @@
+package sign_up
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/go-ushio/ushio/common/put"
+	"github.com/go-ushio/ushio/common/scan"
+	"github.com/go-ushio/ushio/utils/clean"
+)
+
+type SignUp struct {
+	Token     string
+	Email     string
+	CreatedAt time.Time
+	ExpireAt  time.Time
+}
+
+func New(email string, dur time.Duration) *SignUp {
+	now := time.Now()
+	return &SignUp{
+		Email:     clean.String(email),
+		Token:     uuid.New().String(),
+		CreatedAt: now,
+		ExpireAt:  now.Add(dur),
+	}
+}
+
+func ScanSignUp(scanner scan.Scanner) (*SignUp, error) {
+	su := &SignUp{}
+	err := scanner.Scan(&su.Token, &su.Email, &su.CreatedAt, &su.ExpireAt)
+	if err != nil {
+		return &SignUp{}, err
+	}
+	return su, nil
+}
+
+func (su *SignUp) PutWithTokenFirst(putter put.Putter) (sql.Result, error) {
+	return putter.Put(su.Token, su.Email, su.CreatedAt, su.ExpireAt)
+}
+
+func (su *SignUp) Valid() bool {
+	return time.Now().Before(su.ExpireAt)
+}
