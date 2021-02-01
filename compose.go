487ca674f8c914bb1bc8f373b4578bda4b3ebebf
Richard Chen <i@iochen.com>
2021-02-01 12:12:42 +0800

    add compose page
    Signed-off-by: Richard Chen <i@iochen.com>

--- compose.go
+++ compose.go
@@ -1,37 +1,64 @@
 package ushio
 
-//func (ushio *Ushio) ComposePostHandler(ctx *fiber.Ctx) error {
-//		nav, err := ushio.NavFromCtx(ctx)
-//		if err != nil {
-//			return err
-//		}
-//
-//		content, err := mdparse.Parse(ctx.FormValue("compose-content"))
-//		if err != nil {
-//			return err
-//		}
-//
-//		p := &post.Post{
-//			Title:    c.FormValue("title"),
-//			Creator:  nav.User.UID,
-//			Content:  *content,
-//			Markdown: c.FormValue("compose-content"),
-//		}
-//		if err := ushio.Data.InsertPost(p); err != nil {
-//			return err
-//		}
-//
-//		return ctx.Redirect("/", 303)
-//}
-//
-//func (ushio *Ushio) ComposeHandler(ctx *fiber.Ctx) error {
-//		nav, err := ushio.NavFromCtx(ctx)
-//		if err != nil {
-//			return err
-//		}
-//
-//		return ctx.Render("_compose", fiber.Map{
-//			"Nav":    nav,
-//			"Config": config,
-//		}, "_compose", "editor_head", "head", "nav", "footer")
-//}
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/go-ushio/ushio/core/post"
+	"github.com/go-ushio/ushio/utils/mdparse"
+)
+
+func (ushio *Ushio) ComposePostHandler(ctx *fiber.Ctx) error {
+	ushio.Lock.RLock()
+	defer ushio.Lock.RUnlock()
+	nav, err := ushio.NavFromCtx(ctx)
+	if err != nil {
+		return err
+	}
+
+	content, err := mdparse.Parse(ctx.FormValue("compose-content"))
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	p := &post.Post{
+		Info: &post.Info{
+			Title:     ctx.FormValue("title"),
+			Creator:   nav.User.UID,
+			CreatedAt: now,
+			LastMod:   now,
+			Activity:  now,
+		},
+		Content:  *content,
+		Markdown: ctx.FormValue("compose-content"),
+	}
+	if err := ushio.Data.InsertPost(p); err != nil {
+		return err
+	}
+
+	err = ushio.Cache.IndexPostInfoDrop()
+	if err != nil {
+		return err
+	}
+
+	return ctx.Redirect("/", 303)
+}
+
+func (ushio *Ushio) ComposeHandler(ctx *fiber.Ctx) error {
+	// no database writing operations,
+	// lock is unnecessary
+	nav, err := ushio.NavFromCtx(ctx)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Render("compose", fiber.Map{
+		"Meta": Meta{
+			Config:      *ushio.Config,
+			CurrentPage: "home",
+		},
+		"Nav": nav,
+	})
+}
