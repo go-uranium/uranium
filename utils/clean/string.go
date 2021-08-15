743903919baf2d4c458c163637497a6339b8e57c
Richard Chen <i@iochen.com>
2021-08-15 17:55:04 +0800

    divide functions
    Signed-off-by: Richard Chen <i@iochen.com>

--- utils/clean/string.go
+++ utils/clean/string.go
@@ -2,8 +2,14 @@ package clean
 
 import "strings"
 
-// String cleans string by removing white space at leading and trailing
+// Username cleans string by removing white space at leading and trailing
 // and lowercase the string
-func String(str string) string {
+func Username(str string) string {
+	return strings.ToLower(strings.TrimSpace(str))
+}
+
+// Email cleans string by removing white space at leading and trailing
+// and lowercase the string
+func Email(str string) string {
 	return strings.ToLower(strings.TrimSpace(str))
 }
