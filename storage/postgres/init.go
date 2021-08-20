408adc401e5de09db67f0af6ea1a09702bf0912d
Richard Chen <i@iochen.com>
2021-08-20 21:49:20 +0800

    working on postgres db init
    Signed-off-by: Richard Chen <i@iochen.com>

--- storage/postgres/init.go
+++ storage/postgres/init.go
@@ -1,35 +1,79 @@
 package postgres
 
 func (pg *Postgres) Init() error {
+	err := pg.initUser()
+	if err != nil {
+		return err
+	}
 
 }
 
 var SQLInitUser = `CREATE TABLE IF NOT EXISTS "user"(
     	uid SERIAL PRIMARY KEY NOT NULL,
-    	name VARCHAR(30) NOT NULL,     
     	username VARCHAR(20) NOT NULL,
+    	lowercase VARCHAR(20) NOT NULL UNIQUE,
     	electrons int4 NOT NULL,
-    	permission int2 NOT NULL,
-    	created_at TIMESTAMP NOT NULL
+    	admin int2 NOT NULL,
+    	created timestamptz NOT NULL,
+    	deleted BOOLEAN NOT NULL
 	);
 	CREATE TABLE IF NOT EXISTS user_auth(
 		uid SERIAL REFERENCES "user" NOT NULL,
-		email VARCHAR(320) NOT NULL,
-		password CHAR(64) NOT NULL,
-		security_email VARCHAR(320) NOT NULL,
-		locked TIMESTAMP NOT NULL,
+		email VARCHAR(320) NOT NULL UNIQUE,
+		password bytea(32) NOT NULL,
+		security_email VARCHAR(320),
+		two_factor BOOLEAN NOT NULL,
+		locked BOOLEAN NOT NULL,
+		locked_till timestamptz,
 		disabled BOOLEAN NOT NULL
 	);
 	CREATE TABLE IF NOT EXISTS user_profile(
 		uid SERIAL REFERENCES "user" NOT NULL,
-		bio VARCHAR(255) NOT NULL,
-		
+		name VARCHAR(30) NOT NULL,
+		bio VARCHAR(255),
+		location VARCHAR(30),
+		birthday int4,
+		email VARCHAR(320),
+		social text NOT NULL
 	);
 `
 
 func (pg *Postgres) initUser() error {
 	_, err := pg.db.Exec(SQLInitUser)
-	if err != nil {
-		return err
-	}
+	return err
+}
+
+var SQLInitPost = `CREATE TABLE IF NOT EXISTS "user"(
+    	uid SERIAL PRIMARY KEY NOT NULL,
+    	username VARCHAR(20) NOT NULL,
+    	lowercase VARCHAR(20) NOT NULL UNIQUE,
+    	electrons int4 NOT NULL,
+    	admin int2 NOT NULL,
+    	created timestamptz NOT NULL,
+    	deleted BOOLEAN NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS user_auth(
+		uid SERIAL REFERENCES "user" NOT NULL,
+		email VARCHAR(320) NOT NULL UNIQUE,
+		password bytea(32) NOT NULL,
+		security_email VARCHAR(320),
+		two_factor BOOLEAN NOT NULL,
+		locked BOOLEAN NOT NULL,
+		locked_till timestamptz,
+		disabled BOOLEAN NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS user_profile(
+		uid SERIAL REFERENCES "user" NOT NULL,
+		name VARCHAR(30) NOT NULL,
+		bio VARCHAR(255),
+		location VARCHAR(30),
+		birthday int4,
+		email VARCHAR(320),
+		social text NOT NULL
+	);
+`
+
+func (pg *Postgres) initPost() error {
+	_, err := pg.db.Exec(SQLInitPost)
+	return err
 }
