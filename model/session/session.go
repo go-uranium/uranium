9aee9339bc117642ffd4f42c6df6e806ebae9d1c
Richard Chen <i@iochen.com>
2021-08-21 20:39:41 +0800

    update session model
    Signed-off-by: Richard Chen <i@iochen.com>

--- model/session/session.go
+++ model/session/session.go
@@ -4,20 +4,34 @@ import (
 	"time"
 )
 
+const (
+	SESSION_MODE_UNKNOWN int16 = iota
+	SESSION_MODE_USER
+	SESSION_MODE_MODERATOR
+	SESSION_MODE_ADMIN
+)
+
+const (
+	SESSION_MODE_UNKNOWN_STR   = "0"
+	SESSION_MODE_USER_STR      = "1"
+	SESSION_MODE_MODERATOR_STR = "2"
+	SESSION_MODE_ADMIN_STR     = "3"
+)
+
 type Session struct {
 	Token   string
-	UID     int64
+	UID     int32
+	Mode    int16
 	UA      string
 	IP      string
-	Mode    bool
 	Created time.Time
 	Expire  time.Time
 }
 
 type Basic struct {
 	Token  string
-	UID    int64
-	Sudo   bool
+	UID    int32
+	Mode   int16
 	Expire time.Time
 }
 
