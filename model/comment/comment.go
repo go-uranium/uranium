dd1c5cfa0144abdb5e398ab879a7ca64e099792e
Richard Chen <i@iochen.com>
2021-08-21 20:38:12 +0800

    update comment model
    Signed-off-by: Richard Chen <i@iochen.com>

--- model/comment/comment.go
+++ model/comment/comment.go
@@ -4,19 +4,19 @@ import (
 	"html/template"
 	"time"
 
-	"github.com/go-uranium/uranium/model/markdown"
 	"github.com/go-uranium/uranium/model/user"
 )
 
 type Comment struct {
-	CID       int64              `json:"cid"`
-	PID       int64              `json:"pid"`
-	Content   template.HTML      `json:"content"`
-	Markdown  *markdown.Markdown `json:"markdown"`
-	Creator   user.Basic         `json:"creator"`
-	CreatedAt time.Time          `json:"created_at"`
-	LastMod   time.Time          `json:"last_mod"`
-	// uid list
-	VotePos []int64 `json:"vote_pos"`
-	VoteNeg []int64 `json:"vote_neg"`
+	CID      int32         `json:"cid"`
+	PID      int32         `json:"pid"`
+	Creator  user.Basic    `json:"creator"`
+	Content  template.HTML `json:"content"`
+	Modified time.Time     `json:"modified"`
+	Created  time.Time     `json:"created"`
+}
+
+type Statistic struct {
+	VotePos []int32 `json:"vote_pos"`
+	VoteNeg []int32 `json:"vote_neg"`
 }
