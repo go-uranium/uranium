7213435d5d89005b75e102e3f2e42c5b87178293
Richard Chen <i@iochen.com>
2021-08-22 10:38:15 +0800

    new from ground
    Signed-off-by: Richard Chen <i@iochen.com>

--- storage/postgres/category.go
+++ storage/postgres/category.go
@@ -1,26 +1,7 @@
 package postgres
 
-import (
-	"github.com/go-uranium/uranium/model/category"
-)
+import "github.com/go-uranium/uranium/model/category"
 
-var (
-	SQLGetCategories = `SELECT tid, tname, name, color FROM categories;`
-)
-
-func (pg *Postgres) GetCategories() ([]*category.Category, error) {
-	rows, err := pg.db.Query(SQLGetCategories)
-	if err != nil {
-		return nil, err
-	}
-	var cates []*category.Category
-	for rows.Next() {
-		cate := &category.Category{}
-		err := rows.Scan(&cate.TID, &cate.TName, &cate.Name, &cate.Color)
-		if err != nil {
-			return nil, err
-		}
-		cates = append(cates, cate)
-	}
-	return cates, nil
+func (pg *Postgres) Categories() ([]*category.Category, error) {
+	return nil, nil
 }
