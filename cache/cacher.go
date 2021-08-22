317620de57a046a735200dd345db1e96f585db41
Richard Chen <i@iochen.com>
2021-08-22 10:28:01 +0800

    rebuild cacher interface
    Signed-off-by: Richard Chen <i@iochen.com>

--- cache/cacher.go
+++ cache/cacher.go
@@ -2,27 +2,15 @@ package cache
 
 import (
 	"github.com/go-uranium/uranium/model/category"
-	"github.com/go-uranium/uranium/model/post"
-	"github.com/go-uranium/uranium/model/session"
 	"github.com/go-uranium/uranium/model/user"
 )
 
 type Cacher interface {
-	Init() error
+	UserBasicByUID(uid int32) (*user.Basic, error)
+	UserUIDByLowercase(lowercase string) (int32, error)
+	RefreshUserBasicByUID(uid int32) (*user.Basic, error)
+	RefreshUserUIDByLowercase(lowercase string) (int32, error)
 
-	User(interface{}) (*user.User, error)
-	UserDrop(interface{}) error
-	UserDropAll() error
-
-	IndexPostInfo(page int64) ([]*post.Info, error)
-	IndexPostInfoRefresh() error
-
-	Session(token string) (*session.Basic, error)
-	SessionDropAll() error
-
-	Categories() []*category.Category
-	Category(interface{}) *category.Category
-	CategoryRefresh() error
-
-	DropAll() error
+	CategoryByTName(tname string) (*category.Category, error)
+	RefreshCategory() error
 }
