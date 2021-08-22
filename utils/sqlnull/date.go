29dba8d7d907c5ff97870bf3087c293e04f79ac7
Richard Chen <i@iochen.com>
2021-08-22 10:36:02 +0800

    implements the driver.Valuer driver.Scanner interface
    Signed-off-by: Richard Chen <i@iochen.com>

--- utils/sqlnull/date.go
+++ utils/sqlnull/date.go
@@ -1,12 +1,39 @@
 package sqlnull
 
 import (
+	"database/sql"
+	"database/sql/driver"
+
 	"github.com/go-uranium/uranium/utils/type/date"
 )
 
 type Date struct {
 	Date  *date.Date
 	Valid bool
+}
+
+func (d *Date) Scan(value interface{}) error {
+	nullint := sql.NullInt32{}
+	err := nullint.Scan(value)
+	if err != nil {
+		return err
+	}
+
+	if !nullint.Valid {
+		d.Valid = false
+		return nil
+	}
+	d.Date = date.New(nullint.Int32)
+	d.Valid = true
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (d *Date) Value() (driver.Value, error) {
+	if !d.Valid {
+		return nil, nil
+	}
+	return int64(d.Date.Encode()), nil
 }
 
 func (d *Date) MarshalJSON() ([]byte, error) {
