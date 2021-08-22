a00e59c251008ca680c570ad171dd24f89252046
Richard Chen <i@iochen.com>
2021-08-22 16:21:09 +0800

    add cache type for session model
    Signed-off-by: Richard Chen <i@iochen.com>

--- model/session/session.go
+++ model/session/session.go
@@ -12,10 +12,10 @@ const (
 )
 
 const (
-	SESSION_MODE_UNKNOWN_STR   = "0"
-	SESSION_MODE_USER_STR      = "1"
-	SESSION_MODE_MODERATOR_STR = "2"
-	SESSION_MODE_ADMIN_STR     = "3"
+	SESSION_MODE_UNKNOWN_CHAR   = '0'
+	SESSION_MODE_USER_CHAR      = '1'
+	SESSION_MODE_MODERATOR_CHAR = '2'
+	SESSION_MODE_ADMIN_CHAR     = '3'
 )
 
 type Session struct {
@@ -33,6 +33,12 @@ type Basic struct {
 	UID    int32
 	Mode   int16
 	Expire time.Time
+}
+
+type Cache struct {
+	Token string
+	UID   int32
+	Mode  int16
 }
 
 func (sess *Session) Valid() bool {
