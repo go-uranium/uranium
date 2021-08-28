0f73d4350e9b974deb333c490dc8ce2918433f18
Richard Chen <i@iochen.com>
2021-08-28 18:17:57 +0800

    more err definitions on TOKEN && new on USER
    Signed-off-by: Richard Chen <i@iochen.com>

--- error.go
+++ error.go
@@ -7,29 +7,37 @@ import "net/http"
 // unless they have modified their cookies.
 var (
 	// When token is need but no token found.
-	ErrTokenRequired = NewError(http.StatusUnauthorized, "Token required.")
+	ErrTokenRequired = NewError(1006, http.StatusUnauthorized, "Token required.")
 	// When the action requires sudo permission but no sudo token found.
-	ErrSudoTokenRequired = NewError(http.StatusUnauthorized, "Sudo token required.")
+	ErrSudoTokenRequired = NewError(1007, http.StatusUnauthorized, "Sudo token required.")
+	// When the action requires admin permission but no admin token found.
+	ErrAdminTokenRequired = NewError(1008, http.StatusUnauthorized, "Admin token required.")
 	// When the resource is only available to mod/admin, then return this error.
-	ErrNoPermission = NewError(http.StatusForbidden, "No permission to the resource.")
+	ErrNoPermission = NewError(1009, http.StatusForbidden, "No permission to the resource.")
 
 	// When token can be found in user request but cannot be found in database.
-	ErrInvalidToken = NewError(http.StatusUnauthorized, "Invalid token.")
+	ErrInvalidToken = NewError(1010, http.StatusUnauthorized, "Invalid token.")
 	// When token can be found in user request and in database, but has been expired.
-	ErrTokenExpired = NewError(http.StatusUnauthorized, "Token has been expired.")
+	ErrTokenExpired = NewError(1011, http.StatusUnauthorized, "Token has been expired.")
 
 	// When sudo token found in user request but cannot be found in database.
-	ErrInvalidSudoToken = NewError(http.StatusUnauthorized, "Invalid sudo token.")
+	ErrInvalidSudoToken = NewError(1012, http.StatusUnauthorized, "Invalid sudo token.")
 	// When sudo token found in user request and in database, but has been expired.
-	ErrSudoTokenExpired = NewError(http.StatusUnauthorized, "Sudo token has been expired.")
+	ErrSudoTokenExpired = NewError(1013, http.StatusUnauthorized, "Sudo token has been expired.")
 
 	// When mod token found in user request but cannot be found in database.
-	ErrInvalidModToken = NewError(http.StatusUnauthorized, "Invalid mod token.")
+	ErrInvalidModToken = NewError(1014, http.StatusUnauthorized, "Invalid mod token.")
 	// When mod token found in user request and in database, but has been expired.
-	ErrModTokenExpired = NewError(http.StatusUnauthorized, "Mod token has been expired.")
+	ErrModTokenExpired = NewError(1015, http.StatusUnauthorized, "Mod token has been expired.")
 
 	// When admin token found in user request but cannot be found in database.
-	ErrInvalidAdminToken = NewError(http.StatusUnauthorized, "Invalid admin token.")
+	ErrInvalidAdminToken = NewError(1016, http.StatusUnauthorized, "Invalid admin token.")
 	// When admin token found in user request and in database, but has been expired.
-	ErrAdminTokenExpired = NewError(http.StatusUnauthorized, "Admin token has been expired.")
+	ErrAdminTokenExpired = NewError(1017, http.StatusUnauthorized, "Admin token has been expired.")
+)
+
+// ========================= USER =============================
+var (
+	ErrInvalidUID   = NewError(1018, http.StatusBadRequest, "Invalid UID.")
+	ErrUserNotFound = NewError(1019, http.StatusNotFound, "User not found.")
 )
