89493c5628b82b1e6bdfd685a82a18ce693f7f26
Richard Chen <i@iochen.com>
2021-08-28 18:13:48 +0800

    add support for Lowercase
    Signed-off-by: Richard Chen <i@iochen.com>

--- utils/clean/string.go
+++ utils/clean/string.go
@@ -7,6 +7,10 @@ func Username(str string) string {
 	return strings.TrimSpace(str)
 }
 
+func Lowercase(str string) string {
+	return strings.ToLower(strings.TrimSpace(str))
+}
+
 // Name cleans string by removing white space at leading and trailing
 func Name(str string) string {
 	return strings.TrimSpace(str)
